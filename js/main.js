356591bcbf16bc9e4ddfe971915c20d691d20822
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-06-02 18:16:15 +0300

    opasity

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ $('.reviews__slider').slick({
 
 $('.menu__btn').on('click', function(){
   $('.menu__list').toggleClass('menu__list--active');
-
+  $('.menu__btn').toggleClass('menu__btn--active');
 });
 
 });
